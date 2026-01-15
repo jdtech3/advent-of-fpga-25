c677d9101cf11cb6b8a7e6a8d931fd006bdf3370
Joe Dai <dm.joedai@gmail.com>
2026-01-15 05:30:54 +0000

    day1: add input_size script

--- day1/README.md
+++ day1/README.md
@@ -2,6 +2,8 @@
 
 ## Notes
 
-My first thought for "realistic I/O" was UART, but for simplicity sake, I decided to just use the same interface as the template: parallel I/O with minimal control signals.  
+My first thought for "realistic I/O" was UART, but for simplicity sake, I decided to just use the same interface as the template: parallel I/O with minimal control signals.
+
+Mostly to learn OCaml by doing, but also to determine the min. input bit width, I wrote util/input_size.ml script, which dumps the bit width required given an input file.
 
 ## Interface
---
+++ day1/util/input_size.ml
@@ -0,0 +1,35 @@
+open! Core
+
+(* Arg parse values *)
+let usage_msg = "Usage: input_size <input file>"
+let file = ref ""
+let anon_fun fn = file := fn
+
+(* Function that parses a line and returns the max value,
+   meant to be used in List.fold *)
+let max_val acc s =
+  let n = int_of_string ( String.slice s 1 (String.length s) ) in
+  (* print_endline (string_of_int n); *)
+  if n > acc then n else acc;;
+
+(* Main *)
+let () =
+  Arg.parse [] anon_fun usage_msg;
+  print_string "Input file: ";
+  if not (phys_equal !file "") then 
+    print_endline !file 
+  else 
+    (print_endline usage_msg; exit 1);
+
+  (* Read file + calculate *)
+  let lines = In_channel.read_lines !file in
+  let max = List.fold ~init:0 ~f:max_val lines in
+  let min_bitwidth = Int.ceil_log2 max in
+  
+  (* Output *)
+  print_endline ("Max: " ^ string_of_int max);
+  print_endline (
+    "Min. input bit width: " ^ string_of_int min_bitwidth ^
+    " (max representable: " ^ string_of_int (1 lsl min_bitwidth) ^ ")"
+  )
+;;
